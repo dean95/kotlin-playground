f27ad06c968ddc7db315742e7ead13cc29253cb8
Dean Djermanovic <dean.djermanovic@five.agency>
2021-11-08 16:53:06 +0100

    Add selection sort concept

---
+++ src/main/kotlin/algorithmsAndDataStructures/selectionSort/Main.kt
@@ -0,0 +1,25 @@
+package algorithmsAndDataStructures.selectionSort
+
+private fun main() {
+    val input = intArrayOf()
+    println(selectionSort(input).toList())
+}
+
+private fun selectionSort(input: IntArray): IntArray {
+    val mutableInput = input.toMutableList()
+    val newArray = IntArray(input.size)
+
+    repeat(mutableInput.size) {
+        val smallest = findSmallest(mutableInput.toIntArray())
+        newArray[it] = smallest
+        mutableInput.remove(smallest)
+    }
+    return newArray
+}
+
+private fun findSmallest(input: IntArray): Int {
+    var smallest = input.first()
+    input.forEach { if (it < smallest) smallest = it }
+
+    return smallest
+}
